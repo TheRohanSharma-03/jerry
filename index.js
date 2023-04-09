378c585f74b447b3112b0f9824650a96974982e1
Rohan Sharma <89869165+TheRohanSharma-03@users.noreply.github.com>
2023-04-09 03:03:51 -0700

    j1

---
+++ index.js
@@ -0,0 +1,57 @@
+require("./src/db/conn");
+const Student = require("./src/models/models");
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+
+const app = express();
+const port = 5000;
+
+const templatepath = path.join(__dirname,"./templates/views");
+const partialspath = path.join(__dirname,"./templates/partials");
+
+app.use(express.urlencoded({extended:false}));
+app.set("view engine","hbs");
+app.set("views",templatepath);
+hbs.registerPartials(partialspath);
+
+app.get("/",(req,res)=>{
+    res.render("index");
+});
+
+app.post("/data",async(req,res)=>{
+    try{
+        var name = req.body.name;
+        var email = req.body.email;
+        var city = req.body.city;
+        var state = req.body.state;
+        var phone = req.body.phone;
+        var password = req.body.password;
+
+        const data = new Student({
+            name:name,
+            email:email,
+            city:city,
+            state:state,
+            phone:phone,
+            password:password,
+        });
+        const respone = await data.save();
+        console.log(respone);
+        res.render("index");
+    }catch(error){
+        console.log(error);
+    }
+});
+
+app.get("/show",(req,res)=>{
+    const display = async() =>{
+        const data = await Student.find();
+        res.render("showdata",{data:data});
+    }
+    display();
+});
+
+app.listen(port,()=>{
+    console.log(`Server Started http://localhost:${port}/`);
+});
